52b0382478a4bb9e5e96ab6aae5105912da31daf
hugofrelin <anders.frelin19@imperial.ac.uk>
2023-02-26 16:18:16 -0800

    - Aggregation of connected functions to same file

--- R/qc.zscore.R
+++ R/qc.zscore.R
@@ -77,3 +77,48 @@ correct.zscore.signs <- function(zscores, signs.data, metric.col.name, signs.col
 
     return(zscores)
     }
+
+# ' Sign correct statistical scores and sum across for total sample score
+# '
+# ' This function takes a dataframe of all the negative scores and aggregates
+# ' to a form which can be plotted as a a barplot.
+# '
+# ' @param qc.plotting.data A dataframe whose rows are samples and each column a QC metric
+# ' @param filename A filename where to save data. If NULL data will not be saved to file
+# ' @return A dataframe of z-scores for each metric
+# ' @export
+accumulate.zscores <- function(qc.plotting.data, filename = NULL) {
+
+    qc.plot.data.numeric <- all(apply(qc.plotting.data, 2, is.numeric))
+
+    if (!qc.plot.data.numeric) {
+        stop("qc.plotting.data is not fully numeric")
+        }
+
+    barplot.data <- colSums(qc.plotting.data);
+
+    barplot.df <- data.frame(
+        "Sample" = names(barplot.data),
+        "Sum" = barplot_data,
+        stringsAsFactors = FALSE
+        );
+
+    barplot.df <- barplot.df[order(-barplot.df$Sum), ];
+
+    barplot.df$Sample <- factor(
+        x = barplot.df$Sample,
+        levels = barplot.df$Sample
+        );
+
+    if (!is.null(filename)) {
+        write.table(
+            x = qc.plotting.data,
+            file = filename,
+            quote = FALSE,
+            row.names = TRUE,
+            col.names = TRUE
+            );
+        }
+
+    return(barplot.df)
+    }
