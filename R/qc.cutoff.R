ad5f7bf082093dc6b9bb4f94fee5a9105ca18f23
hugofrelin <anders.frelin19@imperial.ac.uk>
2023-04-23 16:10:36 -0700

    - Addition of fit.and.evaluate

---
+++ R/qc.cutoff.R
@@ -0,0 +1,46 @@
+#' Fits the QC data to distributions and returns the KS test result and BIC score
+#'
+#' This function takes the accumulated QC scores, a vector of distributions
+#' and a trimming factor. It then returns the results for each distribution
+#' in a dataframe.
+#'
+#' @param quality.scores The accumulated QC scores, the output of accumulate.zscores
+#' @param distributions A vector of distributions to fit and test
+#' @param trim.factor The fraction of extremes on each end to trim before fitting
+#' @export
+fit.and.evaluate <- function(
+    quality.scores,
+    distributions = c('weibull', 'norm', 'gamma', 'exp', 'lnorm', 'cauchy', 'logis'),
+    trim.factor = 0.05
+    )
+    {
+
+    no.distributions <- length(distributions);
+    ks.rejected <- logical(no.distributions);
+    BIC.value <- numeric(no.distributions);
+
+    no.samples <- nrow(quality.scores);
+    trim.num <- round(no.samples * trim.factor);
+
+    quality.scores <- quality.scores[-((no.samples - trim.num + 1):no.samples), ];
+    quality.scores <- quality.scores[-(1:trim.num), ];
+
+    for (i in seq_len(no.distributions)){
+
+        fit <- fitdistrplus::fitdist(-quality.scores$Sum, distributions[i]);
+        gof <- fitdistrplus::gofstat(fit);
+
+        ks.rejected[i] <- gof$kstest == 'rejected';
+        BIC.value[i] <- fit$bic;
+        }
+
+    dist.df <- data.frame(
+        distributions = distributions,
+        ks.rejected = ks.rejected,
+        BIC.value = BIC.value
+        );
+
+    dist.df <- dist.df[order(dist.df$BIC.value), ];
+
+    return(dist.df);
+    }
