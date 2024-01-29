a137b0de0d9ff8f29cab7ddbc4b7cbd8258d9c24
helen-zhu <helen.m.zhu@gmail.com>
2024-01-29 13:25:21 -0500

    test: adding tests for zscores

--- tests/testthat/test-zscores.R
+++ tests/testthat/test-zscores.R
@@ -26,6 +26,19 @@ signs <- data.frame(
   "Sign" = c("pos", "neg", "pos")
 )
 
+# Corrected signs
+correct_signs <- zscores
+correct_signs[,"B"] <- -correct_signs[,"B"]
+correct_signs[correct_signs > 0 ] <- 0
+
+# Accumulated zscores
+accumulated_scores <- data.frame(
+  "Sample" = rownames(correct_signs),
+  "Sum" = rowSums(correct_signs)
+)
+accumulated_scores <- accumulated_scores[order(accumulated_scores$Sum, decreasing = T),]
+accumulated_scores$Sample <- factor(accumulated_scores$Sample, levels = accumulated_scores$Sample)
+
 test_that("zscore.from.metrics", {
 
   # Expected calculation of z-scores
@@ -58,19 +71,67 @@ test_that("zscore.from.metrics", {
 test_that("correct.zscore.signs", {
 
   # Expected calculation of z-score sign correction
+  res <- correct.zscore.signs(zscores = zscores, signs.data = signs, metric.col.name = "Metric", signs.col.name = "Sign")
+  expect_equal(res, correct_signs)
 
   # Missing columns
+  missing_columns <- signs[1:2,]
+  expect_error(
+    correct.zscore.signs(
+      zscores = zscores,
+      signs.data = missing_columns,
+      metric.col.name = "Metric",
+      signs.col.name = "Sign"
+    )
+  )
+
+  # Incorrect column names
+  expect_error(
+    correct.zscore.signs(
+      zscores = zscores,
+      signs.data = signs,
+      metric.col.name = "M",
+      signs.col.name = "Sign"
+    )
+  )
 
   # Extra columns
+  extra_columns <- rbind(signs, data.frame("Metric" = "K", "Sign" = "pos"))
+  expect_error(
+    correct.zscore.signs(
+      zscores = zscores,
+      signs.data = extra_columns,
+      metric.col.name = "Metric",
+      signs.col.name = "Sign"
+    )
+  )
 
   # Incorrect specification of 'pos' and 'neg'
+  incorrect_specification <- cbind(signs, "Incorrect" = c("+", "-", "+"))
+  expect_error(
+    correct.zscore.signs(
+      zscores = zscores,
+      signs.data = incorrect_specification,
+      metric.col.name = "Metric",
+      signs.col.name = "Incorrect"
+    )
+  )
 
 })
 
 test_that("accumulate.zscores", {
 
   # Expected calculation of accumulation of z-scores
+  res <- accumulate.zscores(zscores.corrected = correct_signs)
+  expect_equal(res, accumulated_scores)
 
   # Missing sample names?
+  missing_sample_names <- correct_signs
+  rownames(missing_sample_names) <- NULL
+  expect_error(
+    accumulate.zscores(
+      zscores.corrected = missing_sample_names
+    )
+  )
 
 })
