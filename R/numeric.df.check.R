32b283eb4344b62ab62afbc3bc8dd5827b66f221
helen-zhu <helen.m.zhu@gmail.com>
2024-01-30 16:09:01 -0500

    feat: adding a function to test for valid quality score inputs

--- R/numeric.df.check.R
+++ R/numeric.df.check.R
@@ -12,3 +12,31 @@ numeric.df.check <- function(dataframe) {
 
     return(NULL);
     }
+
+
+#' Test if a dataframe is the valid output of accumulate.zscores
+#'
+#' @param quality.scores The dataframe one wants to test
+#' @noRd
+accumulate.zscores.output.check <- function(quality.scores) {
+
+    stopifnot(is.data.frame(quality.scores))
+
+    if(!("Sum" %in% colnames(quality.scores)) || !("Sample" %in% colnames(quality.scores))){
+        stop("quality.scores must be a data.frame that contains the columns Sum and Sample");
+        }
+
+    if(!is.numeric(quality.scores[,"Sum"])){
+        stop("The column Sum in quality.scores must be numeric");
+        }
+
+    if(!is.character(quality.scores[,"Sample"]) && !is.factor(quality.scores[,"Sample"])){
+        stop("Sample ids must be character or factor");
+        }
+
+    if(length(quality.scores[,"Sample"]) != length(unique(quality.scores[,"Sample"]))){
+        stop("Sample ids must be unique");
+        }
+
+    return(NULL);
+    }
