046f8863c873f45338591447672360b8cc572819
helen-zhu <helen.m.zhu@gmail.com>
2024-01-30 15:59:22 -0500

    docs: typo

--- R/calculate.cutoff.R
+++ R/calculate.cutoff.R
@@ -1,7 +1,7 @@
 #' Calculates the cutoff for what the largest observed value has to be, in order to be considered an outlier.
 #'
 #' @param theoretical.data.quantile.max The largest theoretical value from quantile estimation
-#' @param cos.sim.cutoff The cosine similarity value which is on the threshold to having a signficant p-value
+#' @param cos.sim.cutoff The cosine similarity value which is on the threshold to having a significant p-value
 #' @noRd
 calculate.cutoff <- function(theoretical.data.quantile.max, cos.sim.cutoff){
     a <- 1 - 2 * cos.sim.cutoff^2;
